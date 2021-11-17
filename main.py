69c27fbe35a7bd53796adcd7a4527de7c94c026d
Krzysztof Banek <krisbanek@gmail.com>
2021-11-17 22:14:14 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+fives = []
+for number in range(0,101):
+    if number % 5 == 0 and number / 5 > 0:
+        fives.append(number)
+cubics = [number * number * number for number in fives]
+print(f" Liczby od 0 do 100 podzielne przez 5: {fives}")
+print(f" Sześciany liczb od 0 do 100 podzielnych przez 5: {cubics}")
