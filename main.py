30cc0e4ec6d4bf26fe95f290bf02719529a9839e
krisbanek <94645752+krisbanek@users.noreply.github.com>
2021-11-18 21:37:27 +0100

    Add print command

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ cubics = [number * number * number for number in fives]
 print(f" Liczby od 0 do 100 podzielne przez 5: {fives}")
 print(f" Sześciany liczb od 0 do 100 podzielnych przez 5: {cubics}")
 #test GitHub
+print("GitHub jest spoczko")
