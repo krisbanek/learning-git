dabd8f38fc888dcc473ea460c6d4d2fe28a57671
krisbanek <94645752+krisbanek@users.noreply.github.com>
2021-11-18 21:34:25 +0100

    Test GitHub

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ for number in range(0,101):
 cubics = [number * number * number for number in fives]
 print(f" Liczby od 0 do 100 podzielne przez 5: {fives}")
 print(f" Sześciany liczb od 0 do 100 podzielnych przez 5: {cubics}")
+#test GitHub
